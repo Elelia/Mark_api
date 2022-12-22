49361f5c8a0147219492c3e058045d606c5445dd
Elelia <llucas@ensup.eu>
2022-12-22 19:05:01 +0100

    update

--- dbconfig.js
+++ dbconfig.js
@@ -4,18 +4,8 @@ const pool = new pg.Pool({
     user: 'lisa',
     host: 'fs405877-002.eu.clouddb.ovh.net',
     database: 'mark',
-    password: 'pinG07unF',
+    password: '*******',
     port: 35274
 });
-
-// const getVideos = (req, res) => {
-//     pool.query('SELECT * FROM video', (err, res) => {
-//         if(err) {
-//             throw err
-//         }
-//         console.log('Bravo !')
-//         // pool.end()
-//     })
-// }
 
 module.exports = pool;
