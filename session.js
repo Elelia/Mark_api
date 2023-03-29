999f7e392e6748faf97ba7dbe288cfad7e674ad9
Elelia <llucas@ensup.eu>
2023-03-29 12:02:47 +0200

    update

--- session.js
+++ session.js
@@ -91,6 +91,23 @@ function clearTokenCookie(req, res) {
   }
 }
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 //quand l'utilisateur se logout
 // function blacklistToken(req, res, next) {
 //   try {
@@ -111,5 +128,6 @@ module.exports = {
   authenticateToken,
   refreshToken,
   setTokenCookie,
-  clearTokenCookie
+  clearTokenCookie,
+  allowCors
 };
--- src/controllers/serie_film.controller.js
+++ src/controllers/serie_film.controller.js
@@ -1,5 +1,11 @@
 const Seriefilm = require('../models/class/serie_film.class');
 const SeriefilmFunction = require('../models/serie_film.model');
+const Session = require('../../session');
+
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
 
 //fonction qui permet de créer tous les utilisateurs et de les retourner
 async function allSeriefilm(req, res) {
@@ -78,6 +84,7 @@ async function allAvis(req, res) {
     const results = await SeriefilmFunction.getAllAvis(req.params.serie_film_id);
     if (results.length > 0) {
         console.log(results);
+        Session.allowCors(handler)
         res.status(200).json(results);
     } else {
         res.status(500).send('No values');
