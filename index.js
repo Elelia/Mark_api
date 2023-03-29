5313057752e707c84fdb4ac1aecc3cf847b8a4d7
Elelia <llucas@ensup.eu>
2023-03-29 11:18:56 +0200

    update

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const cookieParser = require('cookie-parser');
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://mark-website-sigma.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //app.use(cors({
 //  origin: 'http://localhost:3000'
 //}));
