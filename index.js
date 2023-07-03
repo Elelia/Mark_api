57336ca65ae633374cc60088cb749ebd5ec529a9
Lisa <llucas@ensup.eu>
2023-07-03 18:51:44 +0200

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 //app.use(cors());
 //permet d'autoriser le cross plateforme
 //ce qui permet à l'API de communiquer avec le site internet qui n'a pas la même racine
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://mark-website-sigma.vercel.app', credentials: true }));
 
 /*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://mark-website-sigma.vercel.app");
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://mark-website-sigma.vercel.app");
   next();
 });
 
