7ea79341480891f39d52c93ff9ed174bbff4296e
Lisa <llucas@ensup.eu>
2023-07-04 10:04:01 +0200

    update

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.header("Access-Control-Allow-Origin", "https://mark-website-sigma.vercel.app");
   next();
 });
